9ccc180b13dd7cfe6d9c378c653e7b3987533027
mbataprecious <mbataprecious9@gmail.com>
2022-05-28 03:43:32 +0100

    test is working

--- user_data.php
+++ user_data.php
@@ -1,7 +1,13 @@
 <?php
+//getting post data
 $details=$_POST;
+//file name to write to
 $filename="./userdata.csv";
+//open write strean
 $myfile = fopen($filename, "w") or die("Unable to open file!");
+//store in csv
 fputcsv($myfile, $details);
+//close and clean stream 
 fclose($myfile);
+//print array
 print_r($details);
